0f99d7f2c7a335eefc0c96bf672c717e8767bf49
Alex <workernight@yandex.ru>
2022-06-04 20:37:42 +0400

    feat: add some fragments and view pager

---
+++ app/src/main/java/com/example/spacepicture/ui/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.example.spacepicture.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.example.spacepicture.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewPager = view.findViewById<ViewPager>(R.id.view_pager)
+        viewPager.adapter = ViewPagerAdapter(this.childFragmentManager)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = MainFragment()
+    }
+}
